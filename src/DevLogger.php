eb8a9b8897768b7e672d3e2d4ae06ac4240d9561
Etch Akhtar <etch@clearcutcoding.com>
2015-10-23 16:07:30 +0100

    first commit

---
+++ src/DevLogger.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Console logger alternative for server-side
+ *
+ * @author Etch Akhtar
+ * @version 1.0
+ */
+
+namespace ClearCutCoding\WebBundle\Helpers;
+
+use Silex\Application;
+
+class DevLogger
+{
+	protected $handler;
+
+    public function __construct($logFile)
+	{
+		$this->handler = fopen($logFile, 'w');
+	}
+
+    public function __destruct()
+	{
+		fclose($this->handler);
+	}
+
+    public function log($entry)
+	{
+		fwrite($this->handler, $entry . PHP_EOL);
+	}
+
+}
